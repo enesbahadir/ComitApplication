81d00fe35e1810b5591384a3bd6d29e470e6c500
enes.yildirim <enesbahadiryildirim@gmail.com>
2020-11-19 21:03:13 +0300

    Spring security ile authentication eklendi.

---
+++ comit/src/main/java/com/comit/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.comit.security.service;
+
+import com.comit.execption.UserNotFoundException;
+import com.comit.model.User;
+import com.comit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UserNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
