80e7f740192c9fb83ea292a848ea2c289d932732
veliihsanyavcin <1120606032Vi>
2020-11-19 15:43:47 +0300

    Menu page added.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+// import {BrowserAnimationModule} from '@angular/platform-browser/animations'
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
 
@@ -12,19 +12,29 @@ import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
 import { HomeComponent } from './home';
 import { OrderComponent } from './order/order.component';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {_MatMenuDirectivesModule, MatMenuModule} from "@angular/material/menu";
+
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        AppRoutingModule
-    ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatIconModule,
+    _MatMenuDirectivesModule,
+    MatMenuModule,
+    // BrowserAnimationModule
+  ],
     declarations: [
         AppComponent,
         AlertComponent,
         HomeComponent,
-        OrderComponent
+        OrderComponent,
+        MenuComponent
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
