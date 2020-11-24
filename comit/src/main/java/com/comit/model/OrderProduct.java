fdfdf6258c1e5f456472c8b8a67ec7fac806999a
enes.yildirim <enesbahadiryildirim@gmail.com>
2020-11-24 19:12:32 +0300

    Class'ın eskii versiyonunun bulunduğu comment silindi.

--- comit/src/main/java/com/comit/model/OrderProduct.java
+++ comit/src/main/java/com/comit/model/OrderProduct.java
@@ -6,49 +6,7 @@ import javax.persistence.*;
 
 @Entity
 public class OrderProduct {
-/*
-    @Id
-    @GeneratedValue
-    private int id;
 
-    @ManyToOne
-    private Product product;
-
-    @ManyToOne
-    private Order order;
-
-    public OrderProduct() {
-    }
-
-    public OrderProduct(Product product, Order order) {
-        this.product = product;
-        this.order = order;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-*/
     @EmbeddedId
     @JsonIgnore
     private OrderProductPk pk;
