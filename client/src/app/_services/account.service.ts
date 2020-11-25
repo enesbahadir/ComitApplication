13d35d780bbadd110e157bc2c701939318056eb6
veliihsanyavcin <1120606032Vi>
2020-11-25 16:13:39 +0300

    Frontend-Order sayafası tamamlandı.

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -72,7 +72,7 @@ export class AccountService {
             }));
     }
 
-    delete(id: string) {
+    delete(id: number) {
         return this.http.delete(`${environment.apiUrl}/users/${id}`)
             .pipe(map(x => {
                 // auto logout if the logged in user deleted their own record
